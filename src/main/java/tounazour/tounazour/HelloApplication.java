0cc956424cf5c4c98daddca44f66eedb836b4c2f
tfarfaoua <taoufiqfarfaoua38@gmail.com>
2023-11-03 20:28:26 +0100

    .

--- src/main/java/tounazour/tounazour/HelloApplication.java
+++ src/main/java/tounazour/tounazour/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1018, 557);
-        stage.setTitle("Tounazour");
+        stage.setTitle("Tounazourtest");
         stage.setScene(scene);
         stage.show();
     }
