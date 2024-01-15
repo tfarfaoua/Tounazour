f72a9a743a9a05f186271614a7c6af96155dd55a
yassa boukhari <yasmineelboukhari1@gmail.com>
2024-01-15 20:36:11 +0100

    .

--- src/main/java/tounazour/tounazour/Dashboard.java
+++ src/main/java/tounazour/tounazour/Dashboard.java
@@ -3,6 +3,7 @@ package tounazour.tounazour;
 import javafx.fxml.FXML;
 
 import javafx.fxml.Initializable;
+import javafx.scene.chart.*;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -10,6 +11,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class Dashboard implements Initializable {
@@ -21,6 +23,11 @@ public class Dashboard implements Initializable {
     PreparedStatement ps;
     ResultSet rs;
 
+    XYChart.Series set=new XYChart.Series<>();
+    XYChart.Series set2=new XYChart.Series<>();
+
+
+
     @FXML
     private Label employee_id;
     @FXML
@@ -31,6 +38,35 @@ public class Dashboard implements Initializable {
     private Label production_id;
     @FXML
     private Label date_id;
+
+    //-------------------- barchart
+
+    HashMap<String,Integer> barchatinfo=new HashMap<>();
+
+    @FXML
+    private CategoryAxis X;
+
+    @FXML
+    private NumberAxis Y;
+
+    @FXML
+    private BarChart<?, ?> barcharttona;
+
+
+    //-------------------- linechart
+    @FXML
+    private CategoryAxis XL;
+    @FXML
+    private NumberAxis YL;
+
+    @FXML
+    private LineChart<?, ?> LineCharttona;
+
+
+
+    HashMap<String,Integer> linechatinfo=new HashMap<>();
+
+
 
 
     public void navbarhomeinformation(){
@@ -68,6 +104,64 @@ public class Dashboard implements Initializable {
     }
 
 
+    //
+
+    void insertintobarchart(){
+
+        try{
+            ps=con.prepareStatement("SELECT htype,sum(quantity) FROM honey group by htype");
+            rs=ps.executeQuery();
+
+            while (rs.next()){
+                String ns=String.valueOf(rs.getString("htype"));
+                barchatinfo.put(ns,rs.getInt("sum(quantity)"));
+               // System.out.print(rs.getString("htype"));
+              //  System.out.print(rs.getInt("sum(quantity)"));
+            }
+        }catch (Exception e){
+
+        }
+
+    }
+
+    void barchartinformation(){
+
+        this.barchatinfo.forEach((k,v) -> {
+            this.set.getData().add(new XYChart.Data(k,v));
+        });
+
+        this.barcharttona.getData().addAll(this.set);
+    }
+
+    //
+
+
+    void insertintolinechart(){
+
+        try{
+            ps=con.prepareStatement("SELECT sum(Quantité),DateCommande FROM `commande` group by DateCommande");
+            rs=ps.executeQuery();
+
+            while (rs.next()){
+                linechatinfo.put(rs.getString("DateCommande"),rs.getInt("sum(Quantité)"));
+            }
+        }catch (Exception e){
+
+        }
+
+
+    }
+
+    void linechartinformation(){
+
+        this.linechatinfo.forEach((k,v) -> {
+            this.set2.getData().add(new XYChart.Data(k,v));
+        });
+
+        this.LineCharttona.getData().addAll(this.set2);
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
@@ -76,6 +170,12 @@ public class Dashboard implements Initializable {
         }catch (Exception e){
             System.out.println("error connection with sql !!!");
         }
+
+        insertintobarchart();
+        barchartinformation();
+
+        insertintolinechart();
+        linechartinformation();
 
         this.date_id.setText(java.time.LocalDate.now().toString());
 
