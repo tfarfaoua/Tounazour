8efe4e032fdff7508a26962a9b09f88b6b3fde5d
tounarouz <96828108+tousman@users.noreply.github.com>
2024-01-15 19:29:09 +0100

    .

--- src/main/java/tounazour/tounazour/Main.java
+++ src/main/java/tounazour/tounazour/Main.java
@@ -5,44 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
-import javafx.stage.StageStyle;
 
 
 import java.io.IOException;
 
 
 public class  Main extends Application {
-    private double  xOffset=0;
-    private double  yOffset=0;
-
 
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
         Parent root= FXMLLoader.load(getClass().getResource("login.fxml"));
-=======
-      /*  FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 417);
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-       // Image image=new Image(getClass().getResourceAsStream("img/app logo.png"));
-       // stage.getIcons().add(image);
-        stage.setTitle("Tounazour");
-        stage.initStyle(StageStyle.UNDECORATED);
-
-        scene.setOnMousePressed(mouseEvent -> {
-            xOffset=mouseEvent.getScreenX();
-            yOffset=mouseEvent.getScreenY();
-        });
-        scene.setOnMouseDragged(mouseEvent -> {
-            stage.setX(mouseEvent.getSceneX() - xOffset);
-            stage.setY(mouseEvent.getSceneY() - yOffset);
-        });
-
-        stage.setScene(scene);
-        stage.show();
-    }*/
-       Parent root= FXMLLoader.load(getClass().getResource("login.fxml"));
->>>>>>> 611081213aa315f94ee8af801830e53f013bd210
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Honey");
@@ -50,7 +22,6 @@ public class  Main extends Application {
     }
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
